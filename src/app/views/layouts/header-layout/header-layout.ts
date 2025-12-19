9c4779014e3f1468ca87c0272b82eb93a0c17f9f
Siva2357 <sivakurra.ksp2357@gmail.com>
2025-12-19 13:38:58 +0530

    made the publi page repsosnves

--- src/app/views/layouts/header-layout/header-layout.ts
+++ src/app/views/layouts/header-layout/header-layout.ts
@@ -1,71 +1,34 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { Router,RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-header-layout',
-  imports: [CommonModule,RouterModule],
-  templateUrl: './header-layout.html',
-  styleUrl: './header-layout.css',
   standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header-layout.html',
+  styleUrl: './header-layout.css'
 })
 export class HeaderLayout implements OnInit {
 
-  constructor(public router: Router) {}
+  isMobile = false;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
     this.updateView();
   }
-
 
   @HostListener('window:resize')
-  onResize() {
-    this.updateView();
+  updateView() {
+    this.isMobile = window.innerWidth < 1200; // XL breakpoint
   }
 
-
-  private updateView() {
-    this.isMobile = window.innerWidth < 992;
+  goToLogin() {
+    this.router.navigate(['/login']);
   }
 
-  isActive(route: string): boolean {
-    return this.router.url === route;
+  goToSignup() {
+    this.router.navigate(['/sign-up']);
   }
-
-  isMobile: boolean = false;
-showTalentMenu = false;
-showWorkMenu = false;
-showWhyMenu = false;
-
-hideTimeout: any = null;
-
-onMenuEnter(menu: 'talent' | 'work' | 'why') {
-  clearTimeout(this.hideTimeout);
-  this.resetMenus(); // hide all first
-
-  if (menu === 'talent') this.showTalentMenu = true;
-  else if (menu === 'work') this.showWorkMenu = true;
-  else if (menu === 'why') this.showWhyMenu = true;
-}
-
-onMenuLeave() {
-  this.hideTimeout = setTimeout(() => {
-    this.resetMenus();
-  }, 200);
-}
-
-resetMenus() {
-  this.showTalentMenu = false;
-  this.showWorkMenu = false;
-  this.showWhyMenu = false;
-}
-
-
-goToSignupPage(){
-    this.router.navigate(['sign-up']);
-}
-
-goToLoginPage(){
-     this.router.navigate(['login']);
-}
-
 }
