5a8e793b5a5d0e5ef705b3721135ce8c7b2825f2
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-01-25 10:12:04 +0530

    fxied code

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,14 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { appConfig } from './app/app.config';
-import { provideMonacoEditor } from 'ngx-monaco-editor-v2';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-(self as any).MonacoEnvironment = {
-  getWorkerUrl: function (_moduleId: string, label: string) {
-    const base = `${location.origin}/assets/monaco/vs`;
-
-    switch (label) {
-      case 'typescript':
-      case 'javascript':
-        return `${base}/language/typescript/tsWorker.js`;
-      case 'json':
-        return `${base}/language/json/jsonWorker.js`;
-      case 'css':
-        return `${base}/language/css/cssWorker.js`;
-      case 'html':
-        return `${base}/language/html/htmlWorker.js`;
-      default:
-        return `${base}/editor/editor.worker.js`;
-    }
-  }
-};
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...(appConfig.providers ?? []),
-    provideMonacoEditor(),
     provideAnimationsAsync()
   ]
 }).catch(console.error);
