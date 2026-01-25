3f1584461421b538454301e3d253c6698aa10203
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-01-25 10:26:22 +0530

    added deplyment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  apiGatewayUrl: 'https://talent-hub-backend.onrender.com/api',
+  firebase: {
+    apiKey: 'AIzaSyCG05DLoF-NM9hj5Xdfv2OjXhSp9wpUbc0',
+    authDomain: 'talent-hub-257d6.firebaseapp.com',
+    projectId: 'talent-hub-257d6',
+    storageBucket: 'talent-hub-257d6.firebasestorage.app',
+    messagingSenderId: '51480553352',
+    appId: '1:51480553352:web:d384ffa1301f4368837c3d',
+    measurementId: 'G-WWQH152LWR'
+  }
+};
