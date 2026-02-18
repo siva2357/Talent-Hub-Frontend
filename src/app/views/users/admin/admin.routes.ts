d465dcd749d445c3860d9b97daf5e8b1904e10f5
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-02-18 21:52:27 +0530

    added new components

--- src/app/views/users/admin/admin.routes.ts
+++ src/app/views/users/admin/admin.routes.ts
@@ -6,8 +6,10 @@ import { Seekers } from './seekers/seekers';
 import { RecruiterProfilePage } from './recruiter-profile-page/recruiter-profile-page';
 import { SeekerProfilePage } from './seeker-profile-page/seeker-profile-page';
 import { UserProfile } from './user-profile/user-profile';
-
-
+import { Companies } from './companies/companies';
+import { CompanyDetails } from './company-details/company-details';
+import { BlogDetails } from './blog-details/blog-details';
+import { BlogPage } from './blog-page/blog-page';
 
 export const ADMIN_ROUTES: Routes = [
   {
@@ -19,6 +21,10 @@ export const ADMIN_ROUTES: Routes = [
       { path: 'recruiters-list/:id/profile', component: RecruiterProfilePage },
       { path: 'seekers-list', component: Seekers },
       { path: 'seekers-list/:id/profile', component: SeekerProfilePage },
+      { path: 'company-list', component: Companies },
+      { path: 'company-list/:id/details', component: CompanyDetails },
+      { path: 'blog-list', component: BlogPage },
+      { path: 'blog-list/:id/details', component: BlogDetails  },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
     ],
   },
