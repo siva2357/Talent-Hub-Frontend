c3be77bac2566481284d1be17d3e589a19ac3642
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-03-08 11:18:59 +0530

    changed abckend api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'https://talent-hub-backend-51480553352.us-central1.run.app/api',
+  apiGatewayUrl: 'https://talent-hub-backend-git-51480553352.us-central1.run.app/api',
   CHATBOT_API: 'https://rag-chatbot-51480553352.us-central1.run.app/api',
   firebase: {
     apiKey: 'AIzaSyCG05DLoF-NM9hj5Xdfv2OjXhSp9wpUbc0',
