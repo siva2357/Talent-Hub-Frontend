8696cc74b05993123e2145000bd9fd3f5bbf6e1c
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-03-10 17:19:00 +0530

    remvedusned compoenets

--- src/app/views/admin/admin.routes.ts
+++ src/app/views/admin/admin.routes.ts
@@ -3,12 +3,9 @@ import { Admin } from './admin';
 import { Dashboard } from './dashboard/dashboard';
 import { Recruiters } from './recruiters/recruiters';
 import { Seekers } from './seekers/seekers';
-import { RecruiterProfilePage } from './recruiter-profile-page/recruiter-profile-page';
-import { SeekerProfilePage } from './seeker-profile-page/seeker-profile-page';
 import { UserProfile } from './user-profile/user-profile';
 import { Companies } from './companies/companies';
-import { CompanyDetails } from './company-details/company-details';
-import { BlogDetails } from './blog-details/blog-details';
+
 import { BlogPage } from './blog-page/blog-page';
 
 export const ADMIN_ROUTES: Routes = [
@@ -18,13 +15,9 @@ export const ADMIN_ROUTES: Routes = [
     children: [
       { path: 'dashboard', component: Dashboard },
       { path: 'recruiters-list', component: Recruiters },
-      { path: 'recruiters-list/:id/profile', component: RecruiterProfilePage },
       { path: 'seekers-list', component: Seekers },
-      { path: 'seekers-list/:id/profile', component: SeekerProfilePage },
       { path: 'company-list', component: Companies },
-      { path: 'company-list/:id/details', component: CompanyDetails },
       { path: 'blog-list', component: BlogPage },
-      { path: 'blog-list/:id/details', component: BlogDetails  },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
     ],
   },
