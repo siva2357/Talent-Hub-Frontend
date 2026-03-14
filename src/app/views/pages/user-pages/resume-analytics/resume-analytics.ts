bf4e4d53114930bd9ae3054d97bba359d13d6487
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-03-14 10:10:52 +0530

    created UI for h reusm ATS scoring

--- src/app/views/pages/user-pages/resume-analytics/resume-analytics.ts
+++ src/app/views/pages/user-pages/resume-analytics/resume-analytics.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-resume-analytics',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [ CommonModule,RouterModule],
   templateUrl: './resume-analytics.html',
   styleUrl: './resume-analytics.css'
 })
