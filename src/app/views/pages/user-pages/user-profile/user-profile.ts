41300802a2b59a5a5198f12c727acd5d571d8d9f
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-03-16 21:54:09 +0530

    created reusable modal

--- src/app/views/pages/user-pages/user-profile/user-profile.ts
+++ src/app/views/pages/user-pages/user-profile/user-profile.ts
@@ -6,14 +6,16 @@ import { InputFields } from "../../../components/input-fields/input-fields";
 import { Table } from "../../../components/table/table";
 import { TalentCard } from "../../../components/talent-card/talent-card";
 import { JobpostCard } from "../../../components/jobpost-card/jobpost-card";
+import { Modal } from "../../../components/modal/modal";
 
 @Component({
   selector: 'app-user-profile',
-  imports: [CommonModule, Buttons, Badges, InputFields, Table, TalentCard, JobpostCard],
+  imports: [CommonModule, Buttons, Badges, InputFields, Table, TalentCard, JobpostCard, Modal],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css',
     standalone: true,
 })
 export class UserProfile {
+
 
 }
