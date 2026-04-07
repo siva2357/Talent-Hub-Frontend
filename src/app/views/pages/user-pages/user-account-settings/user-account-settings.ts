1a2d95e76e41e9fa5e7450185bf878b4e1caef58
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-04-07 14:41:51 +0530

    designed UI for accoutn settings and account profile

--- src/app/views/pages/user-pages/user-account-settings/user-account-settings.ts
+++ src/app/views/pages/user-pages/user-account-settings/user-account-settings.ts
@@ -1,18 +1,47 @@
 import { Component } from '@angular/core';
 
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { UpdateBasicDetails } from './update-basic-details/update-basic-details';
 import { UpdateProfileDetails } from './update-profile-details/update-profile-details';
 import { UpdateChangePassword } from './update-change-password/update-change-password';
 import { UpdateProfessionalDetails } from './update-professional-details/update-professional-details';
 import { ManageAccount } from './manage-account/manage-account';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../../core/services/auth-service';
+
+
 @Component({
   selector: 'app-user-account-settings',
-  imports: [UpdateBasicDetails,UpdateProfileDetails, UpdateChangePassword,UpdateProfessionalDetails,ManageAccount,RouterModule],
+  imports: [UpdateBasicDetails,UpdateProfileDetails, UpdateChangePassword,UpdateProfessionalDetails,ManageAccount,RouterModule,FormsModule,CommonModule],
   templateUrl: './user-account-settings.html',
   styleUrl: './user-account-settings.css',
     standalone: true,
 })
 export class UserAccountSettings {
+
+    constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+
+   activeTab: string = 'basic';
+
+  setTab(tab: string) {
+    this.activeTab = tab;
+  }
+
+    goBack(): void {
+    const role = this.authService.getRole();
+
+    if (role === 'recruiter') {
+      this.router.navigate(['/user/my-jobposts']);
+    } else if (role === 'jobSeeker') {
+      this.router.navigate(['/user/jobprofile']);
+    } else {
+      this.router.navigate(['/']); // fallback
+    }
+  }
 
 }
--- src/app/views/pages/user-pages/user-profile/user-profile.ts
+++ src/app/views/pages/user-pages/user-profile/user-profile.ts
@@ -1,14 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../../core/services/auth-service';
+import { RecruiterProfileService } from '../../../../core/services/recruiter-profile-service';
+import { SeekerProfileService } from '../../../../core/services/seeker-profile-service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-profile',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css',
-    standalone: true,
 })
-export class UserProfile {
+export class UserProfile implements OnInit {
+  profile: any = null;
+  role: string | null = null;
+  isLoading = false;
+
+  constructor(
+    private recruiterService: RecruiterProfileService,
+    private jobSeekerService: SeekerProfileService,
+    private authService: AuthService,
+     private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.role = this.authService.getRole();
+    this.loadProfile();
+  }
 
 
+      goBack(): void {
+    const role = this.authService.getRole();
+
+    if (role === 'recruiter') {
+      this.router.navigate(['/user/my-jobposts']);
+    } else if (role === 'jobSeeker') {
+      this.router.navigate(['/user/jobprofile']);
+    } else {
+      this.router.navigate(['/']); // fallback
+    }
+  }
+
+
+
+
+  loadProfile(): void {
+    this.isLoading = true;
+
+    if (this.role === 'recruiter') {
+      this.recruiterService.getRecruiterProfile().subscribe({
+        next: (res) => {
+          this.profile = res.data;
+          this.isLoading = false;
+        },
+        error: () => (this.isLoading = false),
+      });
+    } else if (this.role === 'jobSeeker') {
+      this.jobSeekerService.getJobSeekerProfile().subscribe({
+        next: (res) => {
+          this.profile = res.data;
+          this.isLoading = false;
+        },
+        error: () => (this.isLoading = false),
+      });
+    }
+  }
 }
