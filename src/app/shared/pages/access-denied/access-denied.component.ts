42a5b61d13b6d823792e2f2dbcfd47c15f3e53d3
Siva2357 <sivakurra.ksp2357@gmail.com>
2026-05-15 12:41:09 +0530

    added enw page UI diesnged

--- src/app/shared/pages/access-denied/access-denied.component.ts
+++ src/app/shared/pages/access-denied/access-denied.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-access-denied',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, ButtonComponent],
   templateUrl: './access-denied.component.html',
   styleUrl: './access-denied.component.css',
 })
-export class AccessDeniedComponent {
-
-}
+export class AccessDeniedComponent {}
