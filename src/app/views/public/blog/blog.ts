0191d484bf865424ab8903c9901b86e97196129d
siva2357 <sivakurra.ksp2357@gmail.com>
2026-08-11 16:43:42 +0530

    adde dofudanr uidiesng fo rht r pubci page sokay

--- src/app/views/public/blog/blog.ts
+++ src/app/views/public/blog/blog.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blog.html',
   styleUrl: './blog.css'
 })
